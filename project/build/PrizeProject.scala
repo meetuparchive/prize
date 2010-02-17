3746f40221883a7b300e67e6c49419ad17f070f4
Nathan Hamblen <nathanh@meetup.com>
2010-02-16 19:22:40 -0500

    the modern world\!

--- project/build/PrizeProject.scala
+++ project/build/PrizeProject.scala
@@ -1,6 +1,6 @@
 import sbt._
 
-class PrizeProject(info: ProjectInfo) extends spde.SpdeProject(info) {
-  val dispatch = "net.databinder" %% "dispatch-json" % "0.5.2"
+class PrizeProject(info: ProjectInfo) extends DefaultProject(info) with spde.SpdeProject {
+  val dispatch = "net.databinder" %% "dispatch-http-json" % "0.7.0"
   val configgy = "net.lag" % "configgy" % "1.3" intransitive()
 } 
--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,5 +1,6 @@
 import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-	val extract = "net.databinder.spde" % "spde-sbt-plugin" % "0.1.0"
+  val databinder_net = "databinder.net repository" at "http://databinder.net/repo"
+	val extract = "us.technically.spde" % "spde-sbt-plugin" % "0.3.0-SNAPSHOT"
 }
