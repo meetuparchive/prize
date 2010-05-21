f3a5f5834634f25e2df99f88d68a42f7d9cd1d68
Nathan Hamblen <nathanh@meetup.com>
2010-05-21 11:58:34 -0400

    mondernized

--- project/build/PrizeProject.scala
+++ project/build/PrizeProject.scala
@@ -1,6 +1,6 @@
 import sbt._
 
-class PrizeProject(info: ProjectInfo) extends DefaultProject(info) with spde.SpdeProject {
-  val dispatch = "net.databinder" %% "dispatch-http-json" % "0.7.0"
+class PrizeProject(info: ProjectInfo) extends DefaultProject(info) with spde.SpdeProject with sxr.Publish {
+  val dispatch = "net.databinder" %% "dispatch-http-json" % "0.7.3"
   val configgy = "net.lag" % "configgy" % "1.3" intransitive()
 } 
--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -2,5 +2,6 @@ import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val databinder_net = "databinder.net repository" at "http://databinder.net/repo"
-	val extract = "us.technically.spde" % "spde-sbt-plugin" % "0.3.0-SNAPSHOT"
+	val spde_sbt = "us.technically.spde" % "spde-sbt-plugin" % "0.4.1"
+	val sxr_publish = "net.databinder" % "sxr-publish" % "0.1.6"
 }
