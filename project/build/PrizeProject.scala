3b033321c14627d528179e6dcb6d93d9a63a0fc7
Nathan Hamblen <nathanh@meetup.com>
2010-10-07 16:05:15 -0400

    updating of versions

--- project/build/PrizeProject.scala
+++ project/build/PrizeProject.scala
@@ -1,6 +1,5 @@
 import sbt._
 
-class PrizeProject(info: ProjectInfo) extends DefaultProject(info) with spde.SpdeProject with sxr.Publish {
-  val dispatch = "net.databinder" %% "dispatch-http-json" % "0.7.3"
-  val configgy = "net.lag" % "configgy" % "1.3" intransitive()
+class PrizeProject(info: ProjectInfo) extends DefaultProject(info) with spde.SpdeProject {
+  val dispatch = "net.databinder" %% "dispatch-http-json" % "0.7.6"
 } 
--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -2,6 +2,5 @@ import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val databinder_net = "databinder.net repository" at "http://databinder.net/repo"
-	val spde_sbt = "us.technically.spde" % "spde-sbt-plugin" % "0.4.1"
-	val sxr_publish = "net.databinder" % "sxr-publish" % "0.1.6"
+  val spde_sbt = "us.technically.spde" % "spde-sbt-plugin" % "0.4.1"
 }
